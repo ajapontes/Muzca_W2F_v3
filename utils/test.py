53ab9fe1d1cf4216761888d94e16ca586ad87661
Alfredo Aponte <alfredo.aponte@u.icesi.edu.co>
2025-05-30 19:54:55 -0500

    Se borra test.py que no forma parte del proyecto

--- utils/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-data = np.load('./data/mel_fwod_dataset_variable.npz', allow_pickle=True)
-mel = data['mel']
-
-formas = [x.shape for x in mel]
-from collections import Counter
-print(Counter(formas))
